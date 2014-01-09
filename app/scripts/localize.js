de8da65731170dd4656a0e5f42f55455ff6a9871
Edouard Lopez <dev+git@edouard-lopez.com>
2014-01-09 17:35:47 +0100

    chore(clean): remove logging

--- app/scripts/localize.js
+++ app/scripts/localize.js
@@ -6,7 +6,7 @@ $(function(){
     Handlebars.registerHelper('localize', function(msg) {
         var regex = /.*([\w]{2})/;
         var l10n = location.hash.match(regex)[1];
-        console.log(l10n);
+
         return msg[l10n] || msg['en'];
     });
 });
