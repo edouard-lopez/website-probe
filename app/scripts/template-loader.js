722db3362e8d1607475a82e3ed14dd17871d4db1
Edouard Lopez <dev+git@edouard-lopez.com>
2014-01-09 17:57:44 +0100

    fix(tpl): update template anchor to previos refactor

--- app/scripts/template-loader.js
+++ app/scripts/template-loader.js
@@ -4,7 +4,7 @@ $(function(){
     'use strict';
     var compiledTemplate = myApp.Templates['app/templates/screenshots.hbs'];
 
-    $('.screenshots').each(function(i, sheet){
+    $('.screenshot').each(function(i, sheet){
         var url = $(sheet).data('json');
         $.getJSON(url).then(function (json) {
             var tpl = compiledTemplate( json );
