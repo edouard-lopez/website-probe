c516f57094770ab18b588ed94e8b0e5314eaf5b9
Edouard Lopez <dev+git@edouard-lopez.com>
2014-01-10 13:05:48 +0100

    fix(screenshot): solve usemin generation
    I had some conflicting name and misused the usemin block

--- Gruntfile.js
+++ Gruntfile.js
@@ -222,11 +222,15 @@ module.exports = function (grunt) {
                 }
             }
         },
+
+        // Reads HTML for usemin blocks to enable smart builds that automatically
+        // concat, minify and revision files. Creates configurations in memory so
+        // additional tasks can operate on them
         useminPrepare: {
             options: {
                 dest: '<%= yeoman.dist %>'
             },
-            html: '<%= yeoman.app %>/index.html'
+            html: ['<%= yeoman.app %>/{,*/}*.html']
         },
 
         // Performs rewrites based on rev and the useminPrepare configuration
