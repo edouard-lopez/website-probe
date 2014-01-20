df857cee61e8f2f55ef8e9a0868f2b290db4beef
Edouard Lopez <dev+git@edouard-lopez.com>
2014-01-20 17:35:45 +0100

    refactor(localize): use safeString to allow HTML

--- app/scripts/localize.js
+++ app/scripts/localize.js
@@ -7,6 +7,8 @@ $(function(){
         var regex = /.*([\w]{2})/;
         var l10n = location.hash.match(regex)[1];
 
-        return msg[l10n] || msg['en'];
+        var locale = msg[l10n] ? l10n : 'en';
+
+        return new Handlebars.SafeString(msg[locale]);
     });
 });
